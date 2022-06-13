252e2d6928f4c83272edf33ce98c5cd05f409aeb
Margaret McCarthy <76971580+Magsers@users.noreply.github.com>
2022-06-13 10:40:32 +0100

    Change site header.

--- src/components/templateMoviePage/index.js
+++ src/components/templateMoviePage/index.js
@@ -33,7 +33,8 @@ const TemplateMoviePage = ({ movie, children }) => {
   }, []);
 
   return (
-    <>
+    <div className={classes.root}>
+    
       <MovieHeader movie={movie} />
 
       <Grid container spacing={5} style={{ padding: "15px" }}>
@@ -56,7 +57,7 @@ const TemplateMoviePage = ({ movie, children }) => {
           {children}
         </Grid>
       </Grid>
-    </>
+    </div>
   );
 };
 
